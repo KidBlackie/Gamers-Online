ec97553bda13e208a3ab109decaf9fc62e2dd6b5
dawsonmjeane <42549127+dawsonmjeane@users.noreply.github.com>
2018-11-20 14:40:04 -0600

    Add files via upload

---
+++ GamersOnline/GamersOnline/Includes/gamersconstants.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_NAME','gamers_online');
+	define('DB_USER','GamersOnline');
+	define('DB_PASSWORD','GamersOnlineConnect');
+	define('DB_HOST','gamersonline.cv2l7ct2481s.us-east-1.rds.amazonaws.com');
+?>
